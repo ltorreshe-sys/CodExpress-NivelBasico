c9754a5641968ed44b047e1a187350c9723618e1
Óscar Andrés Moreno Díaz <123341926+TresMilDeName22@users.noreply.github.com>
2025-10-03 11:09:43 -0500

    Calculadora Precisa

---
+++ CalculadoraPrecisa.py
@@ -0,0 +1,11 @@
+#Hola, bienvenido a CalculadoraPrecisa.py
+#Este archivo tiene el objetivo de guiarte para que puedas crear y realizar el programa del primer reto del Nivel Básico de CodExpress.
+#Así, ya te puedes guiar en qué archivo puedes trabajar y qué es lo que debes hacer.
+
+#Esto es lo que deberás hacer:
+#Implementa + − × ÷ con redondeo comercial a 2 decimales usando decimal.Decimal y ROUND_HALF_UP. División por cero: imprime ERROR:DIV0.
+
+#Asegúrate de tener las extensiones necesarias para la ejecución de tu código (en Python). 
+#Cuando termines, le haces Commit y sincronizas los cambios a tu repositorio con GitHub. Ya nosotros nos encargaremos del resto.
+
+#Hablaste con el Vice de Computer Society UN. Suerte en el reto :)
